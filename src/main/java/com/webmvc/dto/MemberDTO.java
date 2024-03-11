4733b312eb21561517a5e58101434216eb942ed1
JiniiW <ksjine2@gmail.com>
2024-03-11 12:56:26 +0900

    Add DTO: MemberDTO

--- src/main/java/com/webmvc/dto/MemberDTO.java
+++ src/main/java/com/webmvc/dto/MemberDTO.java
@@ -1,4 +1,20 @@
 package com.webmvc.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MemberDTO {
+    private String id;
+    private String password;
+    private String name;
+    private String email;
+    private LocalDate joinDate;
 }
