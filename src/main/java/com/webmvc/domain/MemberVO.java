e64350a79b39e41a156259c91eca0b4983aa9163
JiniiW <ksjine2@gmail.com>
2024-03-11 12:56:02 +0900

    Add VO: MemberVO

--- src/main/java/com/webmvc/domain/MemberVO.java
+++ src/main/java/com/webmvc/domain/MemberVO.java
@@ -1,4 +1,18 @@
 package com.webmvc.domain;
 
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Builder
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class MemberVO {
+    private String id;
+    private String password;
+    private String name;
+    private String email;
+    private LocalDate joinDate;
 }
